10bcee68f80903d2945002b2ff6c3af163a3eab4
andresum97 <andresum1309@gmail.com>
2017-10-13 09:05:38 -0600

    El primer programa sin cambios

---
+++ Laboratorio 4/src/AlarmaMain.java
@@ -0,0 +1,17 @@
+
+public class AlarmaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**	AlarmaEmergencia alarmaIncendio = new AlarmaEmergencia();
+		alarmaIncendio.m1();                  // emergencia 1
+		alarmaIncendio.m2();                  // alarma 1 / timbre 1
+		System.out.println(alarmaIncendio);   // emergencia Ring…Ring…Ring…Ring
+		*/
+		Alarma miReloj = new Alarma(); 
+		System.out.println(miReloj); 
+		miReloj.m1(); 
+		miReloj.m2();		
+	}
+
+}
