5a1789d453ba476b29d26915e08e10c38d2227bd
andresum97 <andresum1309@gmail.com>
2017-10-13 10:13:25 -0600

    Main de militares

---
+++ Laboratorio 4/src/MilitarMain.java
@@ -0,0 +1,16 @@
+
+public class MilitarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Militar militar = new Militar();
+		Soldado soldado = new Soldado();
+		System.out.println("Horas de trabajo Militar: "+militar.getHoras());
+		System.out.println("Salario Militar: "+militar.getHoras());
+		System.out.println("Salario Militar: "+militar.getDiasVacaciones());
+		System.out.println("Horas de trabajo Soldado: "+soldado.getHoras());
+		System.out.println("Salario Soldado: "+soldado.getHoras());
+		System.out.println("Salario Soldado: "+soldado.getDiasVacaciones());		
+	}
+
+}
